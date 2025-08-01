68a7304ee5ff4c400d6e1ec9242206db3620c1ad
RAGNAR303 <goncalves303.gt@gmail.com>
2025-08-01 15:55:06 -0300

    date all

--- scripts.js
+++ scripts.js
@@ -220,7 +220,7 @@ checkoutBtn.addEventListener("click", function () {
 function checkRestaurantOpen() {
   const data = new Date();
   const hora = data.getHours();
-  return hora >= 14 && hora < 22;
+  return hora >= 18 && hora < 22;
 }
 
 const spanAberto = document.getElementById("date-aberto");
